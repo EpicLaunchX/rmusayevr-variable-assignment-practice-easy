0c4ac4ef0c7f6a748fb7c160eea25e8af9fd261d
rmusayevr <rmusayevr19@gmail.com>
2025-05-18 12:28:36 +0200

    feat: Added print function

--- src/pytemplate/utils/expressions_example.py
+++ src/pytemplate/utils/expressions_example.py
@@ -6,5 +6,7 @@ product_ab: int = a * b
 
 
 def calculate_and_print(a, b) -> None:
-    print(f"Sum of a and b: {a + b}")
-    print(f"Product of a and b: {a * b}")
+    sum_ab = a + b
+    product_ab = a * b
+    print(f"Sum of a and b: {sum_ab}")
+    print(f"Product of a and b: {product_ab}")
